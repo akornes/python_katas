34f8a0f50a1f3a7f972d4a8257bde86ccef041ba
akornes <andrewkornes@gmail.com>
2023-08-22 10:50:41 -0400

    added user choice

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,6 +2,7 @@ import random
 
 user_choice = int(input("Choose 1 for rock, 2 for paper, or 3 for scissors: "))
 opponent_choice = random.randint(1, 3)
+print(f"Opponent chose:  {opponent_choice}")
 
 if user_choice == opponent_choice:
     print("Tie")
