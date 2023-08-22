345dca7d9653441517b8ee0db510a28e0fa551a0
akornes <andrewkornes@gmail.com>
2023-08-22 10:44:55 -0400

    First iteration

---
+++ rock_paper_scissors.py
@@ -0,0 +1,12 @@
+import random
+
+user_choice = int(input("Choose 1 for rock, 2 for paper, or 3 for scissors: "))
+opponent_choice = random.randint(1, 3)
+
+if user_choice == opponent_choice:
+    print("Tie")
+elif user_choice == 1 and opponent_choice == 3:
+    print("You Win")
+else:
+    print("You Lose")
+
